c16f6844987d963df131ebcc2d96153c5d5cfbe3
Vinicius <108474129+esquilo14@users.noreply.github.com>
2024-05-15 10:59:23 -0300

    Vinivus

--- main.js
+++ main.js
@@ -28,13 +28,16 @@ for (let i = 0; i < botoes.length; i++) {
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
     return dias;
-
-  segundos %=60;
+ segundos %=60;
   minutos %= 60;
   horas %=24;
+  if (tempoFinal > 0){
          return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-
-  }
+  
+         } else{
+            return[0,0,0,0];
+         }
+          }
   
   function atualizaCronometro(){
     for (let i=0; i<contadores.length;i++){
